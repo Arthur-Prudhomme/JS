1aa7ecf4e886d0f1acc49622867da385eb588d26
arthur <arthurprudhommemail06@gmail.com>
2022-11-16 22:45:53 +0100

    :sparkles: matches setup

--- script_jeuxNim.js
+++ script_jeuxNim.js
@@ -1,12 +1,41 @@
-function getInput() {
-    let match = document.getElementById("myInput").value;
-    if(containsOnlyNumbers(match) === true){
-        document.getElementById("demo").innerHTML = match + " allumettes";
-    }else{
-        document.getElementById("demo").innerHTML = "Veuillez rentrer un nombre";
-    }
-}
+let matchesTotal
+let matchesCurrent
 
 function containsOnlyNumbers(str) {
     return /^\d+$/.test(str);
-  }
+}
+
+function getInput(){
+    let matchesTotal = document.getElementById("gameLenght").value;
+    if(containsOnlyNumbers(matchesTotal) === true){
+        parseInt(matchesTotal);
+        if(matchesTotal < 1){
+            document.getElementById("totalMatchesOutput").innerHTML = "Veuillez entrer un nombre supérieur à 0";
+        }else{
+            document.getElementById("nextMove").type = 'number';
+            document.getElementById("confirmNextMove").hidden = '';
+
+            matchesCurrent = matchesTotal;
+
+            document.getElementById("totalMatchesOutput").innerHTML = "Taille de la partie : " + matchesTotal + " allumettes";
+            document.getElementById("currentMatchesOutput").innerHTML = "Nombre restant : " + matchesTotal + " allumettes";
+
+            createMatches(matchesCurrent);
+        }
+    }else{
+        document.getElementById("totalMatchesOutput").innerHTML = "Veuillez entrer un nombre";
+    }
+}
+
+function createMatches(int){
+    let container = document.getElementById("matchesContainer");
+    let check = document.querySelectorAll(`[id="matches"]`);
+    if(check){
+        check.forEach(content => content.remove())
+    }
+    for(i = int; i > 0; i--){
+        let box = document.createElement("div");
+        box.id = "matches";
+        container.appendChild(box);
+    }
+}
